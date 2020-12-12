8a444906b4e3f15d81b42b7539c93a34732a5b31
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-12-12 17:43:03 -0500

    second push

--- App.js
+++ App.js
@@ -3,20 +3,22 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Hello, Martin Guo");
+  
+
+  
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title ="Change Text" onPress={() => setOutputText('Hello, Lisa')}/>
+    <View >
+      <View>
+
+      </View>
+      <View>
+
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
